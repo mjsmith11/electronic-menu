96edd01cd6d8581a6d25097566f917600129bdc6
Matt Smith <mjsmith17@bsu.edu>
2017-10-29 12:44:25 -0400

    Create Visual Studio Project

---
+++ electronic-menu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace electronic_menu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

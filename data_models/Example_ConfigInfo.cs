2d6842e74b0afa9843d31c38f026986603545acf
Matt Smith <mjsmith17@bsu.edu>
2017-11-04 16:20:52 -0400

    Add example ConfigInfo.cs

---
+++ data_models/Example_ConfigInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data_models
+{
+/*
+ *   internal class ConfigInfo
+ *  {
+ *      public static string MySqlConnectionString { get; } = "Server=<1>;database=<2>;uid=<3>; pwd=<4>";
+ *  }
+ *
+ *  // mysql parameters
+ *  //<1> - server url or ip
+ *  //<2> - database name
+ *  //<3> - user id
+ *  //<4> - password
+ */
+}

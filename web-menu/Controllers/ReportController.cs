fb92f2d27c2f967dadcf42131fb1a64da0091952
Matt Smith <mjsmith17@bsu.edu>
2017-11-06 22:25:22 -0500

    Add Total Sales Report

---
+++ web-menu/Controllers/ReportController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using data_models;
+using Microsoft.EntityFrameworkCore;
+
+namespace web_menu.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly NetCoreMenuContext _context;
+
+        public ReportController(NetCoreMenuContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult AllSales()
+        {
+            var paidOrders = _context.Orders
+                .AsNoTracking()
+                .Where(o => o.IsPaid)
+                .Include(o => o.OrderItems)
+                .ThenInclude(oi => oi.MenuItem);
+
+            double total = 0;
+            foreach(var order in paidOrders)
+            {
+                foreach (var item in order.OrderItems)
+                    total += (double)item.ExtendedPrice;
+            }
+            ViewData["TotalSales"] = total;
+            return View();
+        }
+    }
+}

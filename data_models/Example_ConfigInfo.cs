4dc957305cab38e79351dca4c3c82ddd6c4e4d71
Matthew Smith <mjsmith92@outlook.com>
2017-11-30 09:44:56 -0500

    Revert changes to Example_ConfigInfo

--- data_models/Example_ConfigInfo.cs
+++ data_models/Example_ConfigInfo.cs
@@ -5,14 +5,16 @@ using System.Text;
 namespace data_models
 {
 
-   internal class ConfigInfo
- {
-      public static string MySqlConnectionString { get; } = "Server=mysql.mattsmithcs.com;database=electronic_menu;uid=menu_user; pwd=ODm3a9xCtBXK";
- }
-/*  // mysql parameters
- *  //<1> - server url or ip
- *  //<2> - database name
- *  //<3> - user id
- *  //<4> - password
- */
+    /*
+     *   internal class ConfigInfo
+     *  {
+     *      public static string MySqlConnectionString { get; } = "Server=<1>;database=<2>;uid=<3>; pwd=<4>";
+     *  }
+     *
+     *  // mysql parameters
+     *  //<1> - server url or ip
+     *  //<2> - database name
+     *  //<3> - user id
+     *  //<4> - password
+     */
 }

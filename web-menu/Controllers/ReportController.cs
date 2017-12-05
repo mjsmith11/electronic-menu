742e532888e7d5196ca12583d409fa8dd1212b8f
hl751549330 <lhao@bsu.edu>
2017-12-05 15:00:40 +0800

    Complete for Sales Sort by OrderID TableID Price and Tips, Complete for Search Sales for custom input.

--- web-menu/Controllers/ReportController.cs
+++ web-menu/Controllers/ReportController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using data_models;
+using data_models.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace web_menu.Controllers
@@ -34,14 +35,70 @@ namespace web_menu.Controllers
             ViewData["TotalSales"] = total;
             return View();
         }
-        public async Task<IActionResult> History()
+        public async Task<IActionResult> History(string sortOrder, string SearchString, string selected)
         {
-
-            var order = await _context.paymentInfors
-                .AsNoTracking()
-                .ToListAsync();
-
-            return View(order);
+            ViewData["priceSort"] = sortOrder == "price" ? "price_desc" : "price";
+            ViewData["tipSort"] = sortOrder == "tip" ? "tip_desc" : "tip";
+            ViewData["OrderSort"] = sortOrder == "order"? "order_desc" : "order";
+            ViewData["timeSort"] = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewData["CurrentFilter"] = SearchString;
+            var payments = from s in _context.paymentInfors
+                         select s;
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                if (selected == "TableID")
+                {
+                    payments = payments.Where(s => s.TableID.ToString().Contains(SearchString));
+                }
+                else
+                {
+                    if(selected == "OrderID")
+                    {
+                        payments = payments.Where(s => s.OrderID.ToString().Contains(SearchString));
+                    }
+                    else
+                    {
+                        if (selected == "Time")
+                        {
+                            payments = payments.Where(s => s.Time.ToString().Contains(SearchString));
+                        }
+                    }
+                }
+                
+                
+                                       
+            }
+            switch (sortOrder)
+            {
+                case "tip_desc":
+                    payments = payments.OrderByDescending(s => s.Tips);
+                    break;
+                case "tip":
+                    payments = payments.OrderBy(s => s.Tips);
+                    break;
+                case "order_desc":
+                    payments = payments.OrderByDescending(s => s.OrderID);
+                    break;
+                case "order":
+                    payments = payments.OrderBy(s => s.OrderID);
+                    break;
+                case "price":
+                    payments = payments.OrderBy(s => s.TotalPrice);
+                    break;
+                case "price_desc":
+                    payments = payments.OrderByDescending(s => s.TotalPrice);
+                    break;
+                case "Date":
+                    payments = payments.OrderBy(s => s.Time);
+                    break;
+                case "date_desc":
+                    payments = payments.OrderByDescending(s => s.Time);
+                    break;
+                default:
+                    payments = payments.OrderBy(s => s.OrderID);
+                    break;
+            }
+            return View(await payments.AsNoTracking().ToListAsync());
         }
 
 
